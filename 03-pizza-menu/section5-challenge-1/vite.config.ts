3ae004c374304944526426126ff7d342230a4a46
Mariko Suzuki <marikosuzuki@foodtravel.co.jp>
2024-02-21 08:14:27 +0900

    first commit

---
+++ 03-pizza-menu/section5-challenge-1/vite.config.ts
@@ -0,0 +1,34 @@
+import {resolve} from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3100,
+  },
+  preview:{
+    port: 8100,
+  },
+  build: {
+    outDir: resolve(__dirname, './dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        '': resolve(__dirname, './src/main.tsx'),
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name?.split('.').at(1) || 'unknown';
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      }
+  }
+}})
