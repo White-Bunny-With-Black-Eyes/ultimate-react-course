d05f3653ad01b817c03ed009c6f8a517db95507a
Mariko Suzuki <marikosuzuki@foodtravel.co.jp>
2024-02-29 14:46:36 +0900

    lecture 49 ternary operator

--- 03-pizza-menu/starter/src/main.tsx
+++ 03-pizza-menu/starter/src/main.tsx
@@ -100,15 +100,22 @@ const Header = () => {
 };
 
 const Menu = () => {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className='menu'>
       <h2>Our menu</h2>
-
-      <ul className='pizzas'>
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className='pizzas'>
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later🐇🐰🐇</p>
+      )}
       {/* <Pizza
         name='Pizza Spinach'
         ingredients='Tomato, mozarella, spinach, and ricotta cheese'
@@ -149,7 +156,16 @@ const Footer = () => {
   // else alert("Sorry we're closed.");
   return (
     <footer className='footer'>
-      {new Date().toLocaleTimeString()} We're currently open!
+      {isOpen ? (
+        <div className='order'>
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className='btn'>Order</button>
+        </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
+      )}
     </footer>
   );
   // return createElement('footer', null, "We're currently open!");
